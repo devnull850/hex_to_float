05dcd3ca7298ca03bb524ff054412a597e991106
devnull850 <dev.null850@gmail.com>
2021-10-02 11:31:24 -0400

    Move conversion to separate function -dev0

--- converter.c
+++ converter.c
@@ -2,19 +2,30 @@
 #include <stdint.h>
 #include <stdio.h>
 
+double convert_hex_bits_to_double(uint64_t, int, int, int, int);
+
 int main(void) {
     uint64_t n;
-    double result, base, divisor;
-    int exp, p;
 
     // prompt user for number
     scanf("%lx", &n);
+
+    printf("0x%lx == %lf\n", n,
+            convert_hex_bits_to_double(n, 51, 62, 52, 63));
+
+    return 0;
+}
+
+double convert_hex_bits_to_double(uint64_t n,
+        int fraction_msb, int exp_msb, int exp_lsb, int sign_bit) {
+    double result, base, divisor;
+    int exp, p;
 
     // bits 0-51 (52 bits) -> fraction
     base = 1.0f;
     divisor = 0.5f; // 2 ^ -1 = 0.5
 
-    for (int i = 51; i >= 0; --i) {
+    for (int i = fraction_msb; i >= 0; --i) {
         if ((n >> i) & 1) { base += divisor; }
         divisor /= 2.0f;
     }
@@ -22,7 +33,7 @@ int main(void) {
     // bits 52-62 (11 bits) -> exponent
     p = 0;
 
-    for (int i = 52, e = 1; i < 63; ++i, e *= 2) {
+    for (int i = exp_lsb, e = 1; i < exp_msb + 1; ++i, e *= 2) {
         if ((n >> i) & 1) { p += e; }
     }
 
@@ -39,9 +50,7 @@ int main(void) {
 
     // bit 63 -> sign bit
     result = base * exp;
-    if ((n >> 63) & 1) { result *= -1; }
+    if ((n >> sign_bit) & 1) { result *= -1; }
 
-    printf("0x%lx == %lf\n", n, result);
-
-    return 0;
+    return result;
 }
